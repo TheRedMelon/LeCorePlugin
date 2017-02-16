817e14b1e77221ab48dfa0bd4e08cbac6cc86f15
TheRedMelon <theredmelonyt@gmail.com>
2017-02-16 01:28:45 +0000

    les get srys

--- LeCorePlugin/src/com/nekomc/leCorePlugin/LeCorePlugin.java
+++ LeCorePlugin/src/com/nekomc/leCorePlugin/LeCorePlugin.java
@@ -1,29 +1,13 @@
 package com.nekomc.leCorePlugin;
-/*
- * Lewis waz here :)
- * he is very sexy
- */
 
-public class LeCorePlugin {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LeCorePlugin extends JavaPlugin {
 	
 	public void onEnable() {
 		
-		// the truth will be returned
-		isGabeAwesome();
-		isLewisAmazing();
+		
 		
 	}
-	
-public String isGabeAwesome() {
-		
-		return "yes";
-		
-	}
-
-
-private boolean isLewisAmazing(){
-	
-	return true;
-}
 	
 }
