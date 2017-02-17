6b7bde55657e7ce733b33fdcb3bb893c613f7727
TheRedMelon <theredmelonyt@gmail.com>
2017-02-17 16:57:06 +0000

    added '@EventHandler' to events.  (Fuck, I'm an idiot)

--- LeCorePlugin/src/com/nekomc/leCorePlugin/event/player/PlayerJoin.java
+++ LeCorePlugin/src/com/nekomc/leCorePlugin/event/player/PlayerJoin.java
@@ -1,6 +1,7 @@
 package com.nekomc.leCorePlugin.event.player;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -10,6 +11,7 @@ public class PlayerJoin implements Listener {
 
 	LeCorePlugin plugin = LeCorePlugin.plugin;
 	
+	@EventHandler
 	public void onPlayerJoin (PlayerJoinEvent e) {
 		
 		plugin.playerCount = Bukkit.getServer().getOnlinePlayers().size();
--- NekoBoard/src/com/nekomc/nekoBoard/event/player/PlayerJoin.java
+++ NekoBoard/src/com/nekomc/nekoBoard/event/player/PlayerJoin.java
@@ -1,5 +1,6 @@
 package com.nekomc.nekoBoard.event.player;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -7,6 +8,7 @@ import com.nekomc.nekoBoard.Hub;
 
 public class PlayerJoin implements Listener {
 
+	@EventHandler
 	public void onPlayerJoin (PlayerJoinEvent e) {
 		
 		new Hub().showPlayer(e.getPlayer().getUniqueId());
