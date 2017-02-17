2bf3964d1ba87951b7361747d77edc457d765d0a
TheRedMelon <theredmelonyt@gmail.com>
2017-02-17 00:19:00 +0000

    adding nekoboard plugin again

---
+++ NekoBoard/src/com/nekomc/nekoBoard/NekoBoard.java
@@ -0,0 +1,15 @@
+package com.nekomc.nekoBoard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NekoBoard extends JavaPlugin {
+
+	public static NekoBoard plugin;
+	
+	public void onEnable() {
+		
+		plugin = this;
+		
+	}
+	
+}
