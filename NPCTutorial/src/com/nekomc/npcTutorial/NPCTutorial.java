161a04fbf051645b7bed2e0e14c2f6fb66e40b52
root <root@Gabe-Laptop>
2017-03-06 23:27:25 +0000

    fghxsgh(ignore this, i cba 2 write a msg)

--- NPCTutorial/src/com/nekomc/npcTutorial/NPCTutorial.java
+++ NPCTutorial/src/com/nekomc/npcTutorial/NPCTutorial.java
@@ -1,9 +1,41 @@
 package com.nekomc.npcTutorial;
 
+import java.io.File;
+import java.util.HashMap;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NPCTutorial extends JavaPlugin {
 
+	PluginManager pm = getServer().getPluginManager();
 	
+	HashMap<String, YamlConfiguration> configFiles = new HashMap<String, YamlConfiguration>();
+	HashMap<UUID, >
+	
+	public void onEnable() {
+		
+		registerConfig();
+		
+	}
+	
+	private void registerConfig() {
+		
+		File configFolder = new File(getDataFolder() + File.separator + "tutorials");
+		
+		if (!configFolder.exists()) {
+			
+			configFolder.mkdir();
+			
+		}
+		
+		for (File file : configFolder.listFiles()) {
+			
+			configFiles.put(file.getName(), YamlConfiguration.loadConfiguration(file));
+			
+		}
+		
+	}
 	
 }
