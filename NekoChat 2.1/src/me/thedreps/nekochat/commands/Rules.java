0ec6472aa03106c4ec82a6742f23499e93bb8fe6
TheRedMelon <theredmelonyt@gmail.com>
2017-05-24 19:37:44 +0100

    added new rules system

--- NekoChat 2.1/src/me/thedreps/nekochat/commands/Rules.java
+++ NekoChat 2.1/src/me/thedreps/nekochat/commands/Rules.java
@@ -1,11 +1,15 @@
 package me.thedreps.nekochat.commands;
 
+import java.util.List;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import me.thedreps.nekochat.NekoChat;
 import net.minecraft.server.v1_12_R1.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
 
@@ -15,10 +19,19 @@ public class Rules implements CommandExecutor{
 		
 		Player p = (Player) sender;
 		
+		List<String> rules = NekoChat.plugin.getConfig().getStringList("Rules");
 		
-		String json = "{\"text\":\"Click here to see the rules.\",\"color\":\"dark_purple\",\"bold\":true,\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://www.nekomc.com/rules\"}}";
+		for (int i = 1; i <= rules.size(); i++) {
+			
+			p.sendMessage(ChatColor.LIGHT_PURPLE + Integer.toString(i) + ") " + rules.get(i - 1));
+			
+		}
+		
+		String json = "{\"text\":\"Click here to see the rules.\",\"color\":\"dark_purple\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://www.nekomc.com/rules\"}}";
 		PacketPlayOutChat packet = new PacketPlayOutChat(ChatSerializer.a(json));
 		((CraftPlayer)p).getHandle().playerConnection.sendPacket(packet);
+		
+		
 		
 		
 		return false;
