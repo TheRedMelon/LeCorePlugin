005f6e8420b2ca5ac650b0a20961f0d6d73d8500
Lewis Mercer <lewismercer@hotmail.co.uk>
2017-05-26 02:50:33 +0100

    Basic functionality

---
+++ NekoMessage/src/me/thedreps/nekomessage/NekoMessage.java
@@ -0,0 +1,21 @@
+package me.thedreps.nekomessage;
+
+import me.thedreps.nekomessage.commands.MessageCmd;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class NekoMessage extends Plugin{
+	
+	public static NekoMessage plugin;
+	
+	@Override
+	public void onEnable(){
+		registerCommands();
+		
+		plugin = this;
+	}
+	
+	private void registerCommands(){
+		getProxy().getPluginManager().registerCommand(this, new MessageCmd());
+	}
+
+}
---
+++ NekoMessage/src/me/thedreps/nekomessage/commands/MessageCmd.java
@@ -0,0 +1,60 @@
+package me.thedreps.nekomessage.commands;
+
+import java.util.Arrays;
+
+import me.thedreps.nekomessage.NekoMessage;
+import me.thedreps.nekomessage.Rank;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class MessageCmd extends Command {
+
+	public MessageCmd() {
+		super("msg", "", "message", "m", "w", "whisper", "t", "tell");
+	}
+	
+	Rank rank = new Rank();
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		
+		ProxiedPlayer player = (ProxiedPlayer) sender;
+		
+		
+		String prefix = ChatColor.DARK_PURPLE + "Msg" + ChatColor.DARK_GRAY + " | " + ChatColor.RESET;
+		ProxyServer proxy = NekoMessage.plugin.getProxy();
+
+		if (args.length < 2) { //Checks for correct syntax
+
+			player.sendMessage(new ComponentBuilder(prefix + "Usage: /msg <player> <message>").create());
+			return;
+		}
+		
+		
+		ProxiedPlayer reciver = proxy.getPlayer(args[0]);
+		
+		if (reciver == null){ //Checks to see if player is online
+			sender.sendMessage(new ComponentBuilder(prefix + "That player could not be found").create());
+			return;
+		}
+		
+		String[] msgArray = Arrays.copyOfRange(args, 1, args.length);
+		String msg = String.join(" ", msgArray);
+		
+		player.sendMessage(new ComponentBuilder(ChatColor.LIGHT_PURPLE + "[" + ChatColor.RESET + "You " + ChatColor.LIGHT_PURPLE + "-> "
+				+ rank.getColoredRank(reciver.getUniqueId()) + ChatColor.RESET + " " + reciver.getDisplayName()
+				+ ChatColor.LIGHT_PURPLE + "] " + ChatColor.RESET
+				+ msg).create());
+		
+		reciver.sendMessage(new ComponentBuilder(ChatColor.LIGHT_PURPLE + "["
+				+ rank.getColoredRank(player.getUniqueId()) + ChatColor.RESET + " " + player.getDisplayName()
+				+ ChatColor.LIGHT_PURPLE + " -> "
+				+ ChatColor.RESET + "You" + ChatColor.LIGHT_PURPLE + "] " + ChatColor.RESET
+				+ msg).create());
+		
+	}
+}
