c53a4be97101e7a2bcf51ea923d3c481a287d0ec
TheRedMelon <theredmelonyt@gmail.com>
2017-05-28 14:44:58 +0100

    more stuff on tree cmd

--- NekoFundamentals/src/com/nekomc/nekoFundamentals/commands/Tree.java
+++ NekoFundamentals/src/com/nekomc/nekoFundamentals/commands/Tree.java
@@ -2,6 +2,8 @@ package com.nekomc.nekoFundamentals.commands;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.TreeType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,26 +17,46 @@ public class Tree implements CommandExecutor {
 		if (!(sender instanceof Player)) {
 			
 			sender.sendMessage(ChatColor.LIGHT_PURPLE + "Please run this commands as a " + ChatColor.DARK_PURPLE + "PLAYER" + ChatColor.LIGHT_PURPLE + "!");
-			return false;
+			return true;
 			
 		}
 		
-		if (args.length > 2) {
+		if (args.length <= 1) {
 			
+			Player p = (Player) sender;
+			Location loc = p.getEyeLocation();
+			TreeType tt = TreeType.TREE;
 			
-			
+			if (args.length == 1) {
+				
+				if (args[0].equalsIgnoreCase("true")) {
+					
+					
+					
+				} else {
+				
+					if (!(loc.getBlock().getType().equals(Material.DIRT)) && !(loc.getBlock().getType().equals(Material.GRASS))) {
+						
+						p.sendMessage(ChatColor.LIGHT_PURPLE + "Tree cannot be placed here, or use: " + ChatColor.DARK_PURPLE + "/tree true");
+						return true;
+						
+					}
+					
+					try {
+						
+						tt = TreeType.valueOf(args[0]);
+					
+					} catch (IllegalArgumentException e) {
+						
+						return false;
+						
+					}
+							
+				}
+				
+			}
+		
 		}
-		
-		if (args.length == 1) {
-			
-			
-			
-		}
-		
-		Player p = (Player) sender;
-		Location loc = p.getEyeLocation();
-		
-		
 		
 		return true;
 		
