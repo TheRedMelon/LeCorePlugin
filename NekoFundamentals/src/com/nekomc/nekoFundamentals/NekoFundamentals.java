e30c02e44398af53938b6e52053a2ced17daa178
TheRedMelon <theredmelonyt@gmail.com>
2017-05-28 19:20:21 +0100

    removed tree cmd cause i cba

--- NekoFundamentals/src/com/nekomc/nekoFundamentals/NekoFundamentals.java
+++ NekoFundamentals/src/com/nekomc/nekoFundamentals/NekoFundamentals.java
@@ -3,7 +3,6 @@ package com.nekomc.nekoFundamentals;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.nekomc.nekoFundamentals.commands.GamemodeCmd;
-import com.nekomc.nekoFundamentals.commands.Tree;
 
 public class NekoFundamentals extends JavaPlugin{
 	
@@ -16,7 +15,6 @@ public class NekoFundamentals extends JavaPlugin{
 	private void registerCommands() {
 		
 		getCommand("gamemode").setExecutor(new GamemodeCmd());
-		getCommand("tree").setExecutor(new Tree());
 		
 	}
 	
