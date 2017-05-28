f0d9f8a391fc191ff8e8648d2ad2dff1780c07a2
Lewis Mercer <lewismercer@hotmail.co.uk>
2017-05-28 12:04:04 +0100

    Added /gamemode <mode>

--- NekoFundamentals/src/com/nekomc/nekoFundamentals/NekoFundamentals.java
+++ NekoFundamentals/src/com/nekomc/nekoFundamentals/NekoFundamentals.java
@@ -2,6 +2,17 @@ package com.nekomc.nekoFundamentals;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.nekomc.nekoFundamentals.commands.GamemodeCmd;
+
 public class NekoFundamentals extends JavaPlugin{
+	
+	public void onEnable(){
+		registerCommands();
+	}
+	
+	
+	private void registerCommands(){
+		getCommand("gamemode").setExecutor(new GamemodeCmd());
+	}
 
 }
