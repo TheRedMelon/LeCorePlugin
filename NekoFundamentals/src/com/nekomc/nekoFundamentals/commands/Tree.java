aea607ba0acfa2220a8e03ad91cfeb27a6792405
TheRedMelon <theredmelonyt@gmail.com>
2017-05-28 18:29:54 +0100

    tree cmd ready for testing

--- NekoFundamentals/src/com/nekomc/nekoFundamentals/commands/Tree.java
+++ NekoFundamentals/src/com/nekomc/nekoFundamentals/commands/Tree.java
@@ -3,6 +3,7 @@ package com.nekomc.nekoFundamentals.commands;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.TreeType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,50 +14,81 @@ public class Tree implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String command, String[] args) {
 		
-//		String prefix = ChatColor.DARK_PURPLE + "Cheat" + ChatColor.DARK_GRAY + " | " + ChatColor.RESET;
-//		String noPerms = prefix + "Sorry you do not have the required permissions";
-//		String usage = prefix + "Usage: /gamemode <gamemode> [player]";
+		String prefix = ChatColor.DARK_PURPLE + "Cheat" + ChatColor.DARK_GRAY + " | " + ChatColor.RESET;
+		String noPerms = prefix + ChatColor.LIGHT_PURPLE + "Sorry you do not have the required permissions";
+		String usage = prefix + ChatColor.LIGHT_PURPLE + "Usage: /tree [tree type] [true]";
 		
-		
-		if (!(sender instanceof Player)) {
+		if (!sender.hasPermission("nf.tree")) {
 			
-			sender.sendMessage(ChatColor.LIGHT_PURPLE + "Please run this commands as a " + ChatColor.DARK_PURPLE + "PLAYER" + ChatColor.LIGHT_PURPLE + "!");
-			return true;
+			sender.sendMessage(noPerms);
+			return false;
 			
 		}
 		
-		if (args.length <= 1) {
+		if (!(sender instanceof Player)) {
+			
+			sender.sendMessage(prefix + ChatColor.LIGHT_PURPLE + "Please run this commands as a " + ChatColor.DARK_PURPLE + "PLAYER" + ChatColor.LIGHT_PURPLE + "!");
+			return false;
+			
+		}
+		
+		if (args.length <= 2) {
 			
 			Player p = (Player) sender;
 			Location loc = p.getEyeLocation();
-//			TreeType tt = TreeType.TREE;
+			TreeType tt = TreeType.TREE;
 			
 			if (args.length == 1) {
 				
 				if (args[0].equalsIgnoreCase("true")) {
 					
-					
+					loc.getWorld().generateTree(loc, tt);
 					
 				} else {
-				
-					if (!(loc.getBlock().getType().equals(Material.DIRT)) && !(loc.getBlock().getType().equals(Material.GRASS))) {
+					
+					if (!loc.getBlock().getType().equals(Material.DIRT) || !loc.getBlock().getType().equals(Material.GRASS)) {
 						
-						p.sendMessage(ChatColor.LIGHT_PURPLE + "Tree cannot be placed here, or use: " + ChatColor.DARK_PURPLE + "/tree true");
-						return true;
+						p.sendMessage(prefix + ChatColor.LIGHT_PURPLE + "A tree can't be placed here!  Try on grass or dirt or use: "  + ChatColor.DARK_PURPLE + "/tree [tree type] true");
+						return false;
 						
 					}
 					
 					try {
 						
-//						tt = TreeType.valueOf(args[0]);
+						tt = TreeType.valueOf(args[0]);
 					
 					} catch (IllegalArgumentException e) {
 						
+						p.sendMessage(usage);
 						return false;
 						
 					}
+					
+					loc.getWorld().generateTree(loc, tt);
 							
 				}
+				
+			} else {
+				
+				if (!args[1].equalsIgnoreCase("true")) {
+					
+					p.sendMessage(usage);
+					return false;
+					
+				}
+				
+				try {
+					
+					tt = TreeType.valueOf(args[0]);
+				
+				} catch (IllegalArgumentException e) {
+					
+					p.sendMessage(usage);
+					return false;
+					
+				}
+				
+				loc.getWorld().generateTree(loc, tt);
 				
 			}
 		
