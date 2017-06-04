79ae24a7b75e788f2132556a7dbc49135e155c1d
Lewis Mercer <lewismercer@hotmail.co.uk>
2017-06-04 11:02:16 +0100

    First upload for NekoEco

---
+++ NekoEco/src/me/thedreps/nekoEco/NekoEco.java
@@ -0,0 +1,15 @@
+package me.thedreps.nekoEco;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NekoEco extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+
+}
