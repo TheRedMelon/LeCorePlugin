e55aa09612ed7e85660417260d10043d107fd702
Lewis Mercer <lewismercer@hotmail.co.uk>
2017-06-09 09:40:09 +0100

    Added support for /bal [Player]

--- NekoEco/src/me/thedreps/nekoEco/commands/BalanceCmd.java
+++ NekoEco/src/me/thedreps/nekoEco/commands/BalanceCmd.java
@@ -1,5 +1,6 @@
 package me.thedreps.nekoEco.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,19 +16,64 @@ public class BalanceCmd implements CommandExecutor{
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
 		String prefix = ChatColor.DARK_PURPLE + "Eco" + ChatColor.DARK_GRAY + " | " + ChatColor.RESET;
-		//String noPerms = prefix + "Sorry you do not have the required permissions";
+		String noPerms = prefix + "Sorry you do not have the required permissions.";
+		String playerNotFound = prefix + "That player could not be found.";
+		String usage = prefix + "Usage: /bal";
+		String usage2 = prefix + "Usage: /bal [player]";
 		
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(prefix + "This command is for players only!");
 			return true;
 		}
 		
+		
 		Player player = (Player) sender;
 		
-		player.sendMessage(prefix + "$" + api.getBal(player.getUniqueId().toString()));
-		
-		
-		return false;
+		if (args.length == 0){
+			
+			if(player.hasPermission("nekoeco.balance")){
+				player.sendMessage(prefix + "$" + api.getBal(player.getUniqueId().toString()));
+				return true;
+			}else{
+				player.sendMessage(noPerms);
+				return true;
+			}
+			
+			
+			
+			
+		}else if(args.length == 1){
+			
+			if(player.hasPermission("nekoeco.balance.others")){
+				Player player2 = Bukkit.getPlayer(args[0]);
+				
+				if(player2 == null){
+					player.sendMessage(playerNotFound);
+					return true;
+				}
+				
+				player.sendMessage(prefix + "$" + api.getBal(player.getUniqueId().toString()));
+				return true;
+			}else{
+				player.sendMessage(noPerms);
+				return true;
+			}
+			
+		}else{
+			
+			if(player.hasPermission("nekoeco.balance")){
+				if(player.hasPermission("nekoeco.balance.others")){
+					player.sendMessage(usage2);
+				}else{
+					player.sendMessage(usage);
+				}
+			}else{
+				player.sendMessage(noPerms);
+			}
+			
+			return true;
+			
+		}
 	}
 
 }
