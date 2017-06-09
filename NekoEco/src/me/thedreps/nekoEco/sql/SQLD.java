f0568b8247690fa0ffb2758cbfe54f2ef00202d6
Lewis Mercer <lewismercer@hotmail.co.uk>
2017-06-09 01:12:10 +0100

    Updated the SQL credentials.

--- NekoEco/src/me/thedreps/nekoEco/sql/SQLD.java
+++ NekoEco/src/me/thedreps/nekoEco/sql/SQLD.java
@@ -4,8 +4,8 @@ public class SQLD {
 	
 	public static String host = "nekomc.com";
 	public static String port = "3306";
-	public static String db = "nekomcco_nicknames";
-	public static String user = "nekomcco_nickn";
-	public static String pw = "tv&$hyBT!iyg";
+	public static String db = "nekomcco_eco";
+	public static String user = "nekomcco_econ";
+	public static String pw = "TgJwyqnHgmua";
 
 }
